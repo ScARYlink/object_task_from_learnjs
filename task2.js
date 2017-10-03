616b59d645bf99b15a951d1635e4400794564e23
ScARYlink <scarylink25@gmail.com>
2017-10-03 14:28:59 +0300

    add func object verification

--- task2.js
+++ task2.js
@@ -2,3 +2,14 @@
 Создайте функцию isEmpty(obj), которая возвращает true,
 если в объекте нет свойств и false – если хоть одно свойство есть.
 */
+
+function isEmpty(obj) {
+    for (var key in obj) {
+        return false;
+    }
+    return true;
+    }
+var schedule = {};
+alert( isEmpty(schedule) ); // true
+schedule["8:30"] = "подъём";
+alert( isEmpty(schedule) ); // false
