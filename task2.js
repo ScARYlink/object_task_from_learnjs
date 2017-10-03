74ee25a15dc6b1f37dbceac766bac244c9e94a97
ScARYlink <scarylink25@gmail.com>
2017-10-03 14:36:09 +0300

    calculation of money

--- task2.js
+++ task2.js
@@ -19,3 +19,16 @@ alert( isEmpty(schedule) ); // false
 Если объект пустой, то результат должен быть 0.
  */
 
+"use strict";
+
+var salaries = {
+    "Вася": 100,
+    "Петя": 300,
+    "Даша": 250
+};
+
+var sum = 0;
+for (key in salaries) {
+    sum += salaries[key];
+}
+console.log(sum);
